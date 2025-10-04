52a56b9322ce81415103736a844ef758b498ab53
sriram-R-codes <sriram.r.sde@gmail.com>
2025-10-04 23:51:24 +0530

    new

--- server.js
+++ server.js
@@ -129,12 +129,12 @@ app.use('*', (req, res) => {
     success: false,
     error: 'Route not found'
   })
-})
+}) 
 
 const PORT = process.env.PORT || 5000
 
 const server = app.listen(PORT, () => {
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+  console.log(`Server running in on port ${PORT}`)
 })
 
 // Handle unhandled promise rejections
